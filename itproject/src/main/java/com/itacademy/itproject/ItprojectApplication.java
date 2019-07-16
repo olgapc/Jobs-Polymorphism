ba1f41c3dd2002bc68169d990be0bb983b5e23a0
xavieroldan <xavieroldan@gmail.com>
2019-07-16 10:24:09 +0200

    First commit Spring structure

---
+++ itproject/src/main/java/com/itacademy/itproject/ItprojectApplication.java
@@ -0,0 +1,13 @@
+package com.itacademy.itproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItprojectApplication.class, args);
+	}
+
+}
