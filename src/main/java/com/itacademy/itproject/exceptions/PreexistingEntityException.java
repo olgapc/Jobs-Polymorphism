135da669c4dec6371a9326ab33da5c233c8a7aac
Ismael <arkarash@gmail.com>
2019-07-22 12:41:19 +0200

    Exceptions

---
+++ src/main/java/com/itacademy/itproject/exceptions/PreexistingEntityException.java
@@ -0,0 +1,10 @@
+package com.itacademy.itproject.exceptions;
+
+public class PreexistingEntityException extends Exception {
+    public PreexistingEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public PreexistingEntityException(String message) {
+        super(message);
+    }
+}
