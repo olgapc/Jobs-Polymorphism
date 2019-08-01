d50ab810609620aac8844f50427702e66b13790e
Sonia Sanchez <soniasanchezfl@gmail.com>
2019-08-02 01:59:34 +0200

    Student Absences API call

--- src/main/java/com/itproject/application/dto/StudentAbsenceDTO.java
+++ src/main/java/com/itproject/application/dto/StudentAbsenceDTO.java
@@ -2,15 +2,23 @@ package com.itproject.application.dto;
 
 import java.time.LocalDate;
 
+import com.google.gson.annotations.Expose;
 import com.itproject.domain.Student;
 import com.itproject.domain.StudentAbsence;
+import com.itproject.utilities.NotFoundException;
 
 public class StudentAbsenceDTO {
 	
 	private Student student;
+	@Expose
 	private String date;
 	
-	public StudentAbsenceDTO(StudentAbsence studentAbsence) {
+	public StudentAbsenceDTO(StudentAbsence studentAbsence) throws NotFoundException {
+		
+		if (studentAbsence == null) {
+			throw new NotFoundException();
+		}
+		
 		this.student = studentAbsence.getStudent();
 		this.date = studentAbsence.getDate().toString();
 	}
